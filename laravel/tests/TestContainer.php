376d8f31fd8051aaad24477115f78344f2f91ab0
hexing <he.xing@9douyu.com>
2016-05-27 14:02:06 +0800

    test app

--- laravel/tests/TestContainer.php
+++ laravel/tests/TestContainer.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Container\Container;
+use Illuminate\Foundation\Application;
 /**
  * Class TestContainer
  */
@@ -11,7 +12,23 @@ class TestContainer extends PHPUnit_Framework_TestCase
      * 测试容器
      */
     public function testContainer(){
-        $Container = new Container();
-        d($Container);
+        $container = new Container();
+
+        d($container);
+    }
+
+    /**
+     * 项目实例
+     */
+    public function testApplication(){
+        $app_construct_argument = realpath(__DIR__.'/../app/');
+
+        d($app_construct_argument);
+
+        $app = new Application(
+            $app_construct_argument
+        );
+
+        d($app);
     }
 }
